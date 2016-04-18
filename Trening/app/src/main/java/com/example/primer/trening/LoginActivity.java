cfb09cf5d57296f03d5a83a01ae756c6cc85966e
Fredrik <Fredrik@Vatsendvik.com>
2016-04-18 15:06:44 +0200

    login stuff

--- Trening/app/src/main/java/com/example/primer/trening/LoginActivity.java
+++ Trening/app/src/main/java/com/example/primer/trening/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.primer.trening;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 
